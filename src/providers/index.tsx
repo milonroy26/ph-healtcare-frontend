369107755686424fa73be51913c4175f6fd816d7
milonroy26 <milonchandro35@gmail.com>
2026-09-09 10:07:47 +0600

    2nd commit

---
+++ src/providers/index.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+export default function Providers({ children }: { children: ReactNode }) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
